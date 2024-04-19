80098b82bdef26cf6a61ecb10ac793af00054b2c
Sandeep-508 <elsenothing321@gmail.com>
2024-04-19 10:13:54 +0530

    completed the gif image section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,28 @@
-// $(document).ready(function () {
-//     $('#nav-icon2').click(function () {
-//         $(this).toggleClass('open');
-//     });
-// });
-
 let nav_btn = document.querySelector("#nav-icon2");
 let lg_view = document.querySelector(".lg_view");
-
+let body = document.body;
 nav_btn.addEventListener("click", () => {
     nav_btn.classList.toggle("open");
     if (nav_btn.classList.contains("open")) {
         lg_view.classList.add("show");
+        body.style.overflow = "hidden";
     } else {
         lg_view.classList.remove("show");
+        body.style.overflow = "auto";
     }
 });
+
+// var gif = document.getElementById('gif');
+// var isPlaying = false;
+
+// function play_video() {
+//     if (isPlaying) {
+//         gif.pause();
+//         isPlaying = false;
+//         document.querySelector('.play_back p').textContent = "Play Video";
+//     } else {
+//         gif.play();
+//         isPlaying = true;
+//         document.querySelector('.play_back p').textContent = "Pause Video";
+//     }
+// }
