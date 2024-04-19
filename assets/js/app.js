89b76c6c35d0fc90e88d44ec5745dfdffb2d9cd4
Sandeep-508 <elsenothing321@gmail.com>
2024-04-19 11:53:15 +0530

    completed the tab section

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,3 +26,42 @@ nav_btn.addEventListener("click", () => {
 //         document.querySelector('.play_back p').textContent = "Pause Video";
 //     }
 // }
+
+let tab_btn_1 = document.querySelector(".tab_btn_1");
+let tab_btn_2 = document.querySelector(".tab_btn_2");
+let tab_btn_3 = document.querySelector(".tab_btn_3");
+let text_change = document.querySelector(".changing_text");
+let tab_cont = document.querySelectorAll(".tab_cont");
+let tab_cont_1 = document.querySelector(".tab_cont");
+
+
+tab_cont.forEach(item => {
+    let btn = item.querySelector(".tab_btn");
+    btn.addEventListener("click", () => {
+        tab_cont.forEach(other => {
+            if (other !== item) {
+                other.classList.remove("full");
+            }
+        });
+        item.classList.add("full");
+    });
+});
+
+tab_btn_1.addEventListener("click", () => {
+    text_change.innerHTML = `The Julian
+                            Project offers academic opportunities beyond theclassroom for aspiring historians
+                            to dive deeper into
+                            historical research.Our goal is to make the study of history more accessible to all.
+                            Especiallyfor First - Generation and Low - Income(FGLI) students, we offer free, individual mentorship throughout the year to help
+                            guide you on thisenriching journey.We rely on donations to make these opportunitiespossible and hope you could
+                            contribute to The Julian Project.`;
+});
+
+tab_btn_2.addEventListener("click", () => {
+    text_change.innerHTML = `Embarking on writing an extensive history research paper offers an immersive journey into the depths of the past.`;
+});
+
+tab_btn_3.addEventListener("click", () => {
+    text_change.innerHTML = `The Julian Project annually awards prizes to the authors of that year’s
+most distinguishing papers.`;
+});
