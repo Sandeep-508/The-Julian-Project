9ecc54bd039f5ed2a9a5db19da4b9dc5b1417969
Sandeep-508 <elsenothing321@gmail.com>
2024-04-24 16:39:37 +0530

    added scroll to top and progress bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -209,3 +209,36 @@ accordion.forEach(accord_itm => {
         }
     });
 });
+
+
+// scroll to top
+
+let scroll_btn = document.querySelector(".scroll_top");
+let scroll_pt = 200;
+scroll_btn.classList.add("d-none");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > scroll_pt) {
+        scroll_btn.classList.remove("d-none");
+    } else {
+        scroll_btn.classList.add("d-none");
+    }
+    scroll_btn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+});
+
+
+// progress
+
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
